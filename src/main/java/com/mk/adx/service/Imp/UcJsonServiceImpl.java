6dc589184620273679087a959258d37b38c54ad7
毛钟晟 <18636392387@163.com>
2022-07-06 15:54:01 +0800

    uc

--- src/main/java/com/mk/adx/service/Imp/UcJsonServiceImpl.java
+++ src/main/java/com/mk/adx/service/Imp/UcJsonServiceImpl.java
@@ -177,12 +177,11 @@ public class UcJsonServiceImpl implements UcJsonService {
                     tzImage.setH(720);
                 }
                 list.add(tzImage);
-
+                tb.setImages(list);
                 //上游没返回adtype，则用请求type判断广告类型
                 if ("1".equals(request.getImp().get(i).getSlot_type()) || "2".equals(request.getImp().get(i).getSlot_type())) {//信息流或banner
                     tb.setAd_type(1);//原生-广告素材类型
                 }else {
-                    tb.setImages(list);
                     tb.setAd_type(2);//开屏-广告素材类型
                 }
                 tb.setTitle(imp.getJSONObject(i).getJSONArray("ad").getJSONObject(0).getJSONObject("ad_content").getString("title"));//标题
